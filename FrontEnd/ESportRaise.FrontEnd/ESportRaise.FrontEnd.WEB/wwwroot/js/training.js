d0acfe9ee8eb9f280fe2fb3dfbdc984380f96c3b
EdShel <eduardchelemetjev@gmail.com>
2020-11-18 12:05:33 +0200

    Playing first video for first critical moment of a training

--- FrontEnd/ESportRaise.FrontEnd/ESportRaise.FrontEnd.WEB/wwwroot/js/training.js
+++ FrontEnd/ESportRaise.FrontEnd/ESportRaise.FrontEnd.WEB/wwwroot/js/training.js
@@ -3,28 +3,66 @@
     data: {
         trainingId: trainingId,
         criticalMoments: [],
-        currentVideoStreamId: 0,
-        videoStreams: []
+        videoStreams: [],
+
+        currentCriticalMomentIndex: criticalMomentIndex,
+        currentVideoIndex: -1,
     },
     computed: {
-        currentVideo() {
-            return this.videoStreams[this.currentVideoStreamId];
-        },
-        currentVideoSpans() {
-            let spans = [];
-            let videoBegin = new Date(this.currentVideo.startTime);
-            for (let i = 0; i < this.criticalMoments.length; i++) {
-                let criticalMoment = this.criticalMoments[i];
-
-                console.log(222);
-
-                spans.push({
-                    begin: Math.round((new Date(criticalMoment.begin) - videoBegin) / 1000),
-                    end: Math.round((new Date(criticalMoment.end) - videoBegin) / 1000)
-                });
+        currentCriticalMoment() {
+            if (this.currentCriticalMomentIndex === -1) {
+                return null;
             }
 
-            return spans;
+            return this.criticalMoments[this.currentCriticalMomentIndex]
+        },
+        videosForMoment() {
+            if (this.currentCriticalMomentIndex === -1) {
+                return [];
+            }
+            let minTime = new Date(this.currentCriticalMoment.begin);
+            let maxTime = new Date(this.currentCriticalMoment.end);
+            let videosThatMatch = [];
+            for (let video of this.videoStreams) {
+                if (new Date(video.startTime) <= minTime && maxTime <= new Date(video.endTime)) {
+                    videosThatMatch.push(video);
+                }
+            }
+            return videosThatMatch;
+        },
+        currentVideo() {
+            if (this.currentCriticalMoment
+                && this.videosForMoment.length > 0
+                && this.videosForMoment.length > this.currentVideoIndex) {
+                return this.videosForMoment[this.currentVideoIndex];
+            }
+            return null;
+        },
+        momentVideoInterval() {
+            if (!this.currentVideo) {
+                return null;
+            }
+
+            let videoBegin = new Date(this.currentVideo.startTime);
+            let criticalMoment = this.currentCriticalMoment;
+
+            return {
+                begin: Math.round((new Date(criticalMoment.begin) - videoBegin) / 1000),
+                end: Math.round((new Date(criticalMoment.end) - videoBegin) / 1000)
+            };
+        },
+        restartVideoUrl() {
+            if (!this.currentVideo) {
+                return;
+            }
+            let interval = this.momentVideoInterval;
+
+            let videoUrl = '//www.youtube.com/embed/' + this.currentVideo.streamId;
+            let playerParams = '?rel=0&autoplay=1';
+            let spanParams = '&start=' + interval.begin + '&end=' + interval.end;
+            let langParam = '&hl=en';
+
+            return videoUrl + playerParams + spanParams + langParam;
         }
     },
     mounted: function () {
@@ -37,6 +75,9 @@
             }).then(r => {
                 let data = r.data;
                 this.criticalMoments = data.moments;
+                if (this.criticalMoments.length > 0) {
+                    this.currentCriticalMomentIndex = 0;
+                }
 
                 this.getVideos();
             }).catch(e => {
@@ -50,10 +91,15 @@
                 let data = r.data;
                 this.videoStreams = data.streams;
 
-                this.currentVideoStreamId = 0;
+                if (this.videoStreams.length > 0) {
+                    this.currentVideoIndex = 0;
+                }
             }).catch(e => {
 
             });
+        },
+        secondsToHHMMSS(secs) {
+            return new Date(secs * 1000).toISOString().substr(11, 8);
         }
     }
 });
