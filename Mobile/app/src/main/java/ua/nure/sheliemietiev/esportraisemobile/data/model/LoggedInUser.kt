2436a40f0df6ee33f3ec02f48ae263bfa741c5c3
EdShel <eduardchelemetjev@gmail.com>
2020-11-23 09:33:31 +0200

    Setup mobile app

---
+++ Mobile/app/src/main/java/ua/nure/sheliemietiev/esportraisemobile/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package ua.nure.sheliemietiev.esportraisemobile.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+        val userId: String,
+        val displayName: String
+)
---
+++ Mobile/app/src/main/java/ua/nure/sheliemietiev/esportraisemobile/ui/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package ua.nure.sheliemietiev.esportraisemobile.ui.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import ua.nure.sheliemietiev.esportraisemobile.data.LoginDataSource
+import ua.nure.sheliemietiev.esportraisemobile.data.LoginRepository
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                    loginRepository = LoginRepository(
+                            dataSource = LoginDataSource()
+                    )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
