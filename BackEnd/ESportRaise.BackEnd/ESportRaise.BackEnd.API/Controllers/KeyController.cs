f5c89626e109b234332c960d0801e779cc50c0de
EdShel <eduardchelemetjev@gmail.com>
2020-11-29 16:05:24 +0200

    Added youtube key giving endpoint

---
+++ BackEnd/ESportRaise.BackEnd/ESportRaise.BackEnd.API/Controllers/KeyController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace ESportRaise.BackEnd.API.Controllers
+{
+    [Route("[controller]"), ApiController, Authorize]
+    public class KeyController : Controller
+    {
+        private readonly IConfiguration config;
+
+        public KeyController(IConfiguration config)
+        {
+            this.config = config;
+        }
+
+        [HttpGet("youtube")]
+        public IActionResult GetYouTubeKey()
+        {
+            return Json(new
+            {
+                Key = config.GetValue<string>("YouTubePlayerKey")
+            });
+        }
+    }
+}
